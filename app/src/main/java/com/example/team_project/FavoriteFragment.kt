49ea7ff6308063d1ce5fbf8ce807d3716adb24a0
ymj10003 <ymj10003@gmail.com>
2023-11-10 23:16:02 +0900

    단골식당 화면 표시 안되는 점 수정, firebase 연결

--- app/src/main/java/com/example/team_project/FavoriteFragment.kt
+++ app/src/main/java/com/example/team_project/FavoriteFragment.kt
@@ -17,18 +17,27 @@ class FavoriteFragment : Fragment() {
         Favorite_restaurant("부탄츄", food_type.Japanese, location.홍대),
         Favorite_restaurant("프로메사", food_type.western, location.행신),
         Favorite_restaurant("꼬기꼬기", food_type.Korean, location.화전),
-        Favorite_restaurant("맘스터치", food_type.Fastfood, location.배달)
-
+        Favorite_restaurant("맘스터치", food_type.Fastfood, location.배달),
+        Favorite_restaurant("로쏘", food_type.western, location.홍대),
+        Favorite_restaurant("막저", food_type.Korean, location.행신),
+        Favorite_restaurant("쭈꾸미삼겹살", food_type.Korean, location.화전)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentFavoriteBinding.inflate(layoutInflater)
-
-
         binding.recFavorite.layoutManager = LinearLayoutManager(context)
         binding.recFavorite.adapter = FavoriteAdapter(restaurants)
-}
+        return binding.root
+    }
 
 
 
