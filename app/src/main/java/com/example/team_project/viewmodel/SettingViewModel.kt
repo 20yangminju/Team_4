8240a8c3fe4f43b892224ce63b8cb7dddc7b2788
ParkSangsin <pss6039@naver.com>
2023-11-13 18:04:44 +0900

    SettingFragment 오류 수정

--- app/src/main/java/com/example/team_project/viewmodel/SettingViewModel.kt
+++ app/src/main/java/com/example/team_project/viewmodel/SettingViewModel.kt
@@ -7,11 +7,12 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class SettingViewModel : ViewModel() {
-    private val _local = MutableLiveData<ArrayList<Boolean>>(arrayListOf(true, true, true))
-    // [화전, 홍대, 행신]
-    private val _food = MutableLiveData<ArrayList<Boolean>>(arrayListOf(true, true, true, true))
-    // [한식, 중식, 일식, 양식]
+    private val _favorite = MutableLiveData(true)
 
+    private val _local = MutableLiveData(arrayListOf(true, true, true))
+    // [화전, 홍대, 행신]
+    private val _food = MutableLiveData(arrayListOf(true, true, true, true))
+    // [한식, 중식, 일식, 양식]
     val local: LiveData<ArrayList<Boolean>> get() = _local
     val food: LiveData<ArrayList<Boolean>> get() = _food
 
@@ -22,7 +23,7 @@ class SettingViewModel : ViewModel() {
     val isKorean get() = _food.value?.get(0) == true
     val isChinese get() = _food.value?.get(1) == true
     val isJapanese get() = _food.value?.get(2) == true
-    val isWestern get() = _food.value?.get(2) == true
+    val isWestern get() = _food.value?.get(3) == true
 
     fun setHwa(newValue: Boolean) {
         _local.value?.let {
