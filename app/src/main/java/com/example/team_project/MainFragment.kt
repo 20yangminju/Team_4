1a2c5395a00024cafec70ba5029cd1607c228103
ymj10003 <ymj10003@gmail.com>
2023-12-05 23:01:09 +0900

    세팅에 따른 랜덤 추출 구현

--- app/src/main/java/com/example/team_project/MainFragment.kt
+++ app/src/main/java/com/example/team_project/MainFragment.kt
@@ -6,33 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.team_project.databinding.FragmentMainBinding
+import com.example.team_project.viewmodel.SettingViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.values
 
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 class MainFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     var binding: FragmentMainBinding? = null
     private lateinit var databaseReference: DatabaseReference
+    private val viewModel: SettingViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -41,20 +39,50 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater)
         databaseReference = FirebaseDatabase.getInstance().getReference("restaurant")
+
+
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+
+
         binding?.RandomGet?.setOnClickListener {
                 databaseReference.addListenerForSingleValueEvent(object : ValueEventListener{
                     override fun onDataChange(snapshot: DataSnapshot){
-                        val keys = snapshot.children.map {it.key}.toList()
-                        val randomKey = keys.random()
-                        Log.d("FirebaseData", "Randomly selected restaurantKey: $randomKey")
-                        val bundle = Bundle()
-                        bundle.putString("key", randomKey)
-                        findNavController().navigate(R.id.action_mainFragment_to_restaurantFragment, bundle)
+                        val keys = snapshot.children.map { it.key }.toMutableList()
+                        val filteredKeys = keys.filter { key ->
+                            val isfavorite = snapshot.child(key.toString()).child("isfavorite").getValue() as? Boolean
+                            val delivery = snapshot.child(key.toString()).child("info").child("delivery").value as? Boolean
+                            val foodType = snapshot.child(key.toString()).child("info").child("foodtype").getValue()?.toString()
+                            val location = snapshot.child(key.toString()).child("info").child("location").getValue()?.toString()
+
+                            val isFavoriteCheck = isfavorite != null && viewModel.isFavor && isfavorite == false
+                            val deliveryCheck = delivery != null && viewModel.isDelivery && delivery == false
+                                // 조건에 맞지 않으면 true 반환하여 리스트에 포함되지 않도록 함.
+                            !(!viewModel.isJapanese && foodType == "japanese" ||
+                                    !viewModel.isKorean && foodType == "korean" ||
+                                    !viewModel.isChinese && foodType == "chinese" ||
+                                    !viewModel.isWestern && foodType == "western" ||
+                                    !viewModel.isFast && foodType == "fastfood" ||
+                                    isFavoriteCheck ||
+                                    deliveryCheck ||
+                                    !viewModel.isHaeng && location == "행신" ||
+                                    !viewModel.isHong && location == "홍대" ||
+                                    !viewModel.isHwa && location == "화전" ||
+                                    key == "Error"
+                                    )
+                            }
+
+
+
+
+                            val randomKey = filteredKeys.randomOrNull() ?: "Error"
+                            val bundle = Bundle()
+                            bundle.putString("key", randomKey)
+                            findNavController().navigate(R.id.action_mainFragment_to_restaurantFragment, bundle)
+
                     }
 
                     override fun onCancelled(error: DatabaseError) {
@@ -76,23 +104,4 @@ class MainFragment : Fragment() {
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
